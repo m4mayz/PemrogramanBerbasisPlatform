51e5cfd9459f71d8f378f0a4ca7cbc2a173bd6c9
m4mayz <akmalzaidan960@gmail.com>
2024-12-18 23:43:29 +0700

    tes

--- session9/userProfile/routes/user-route.js
+++ session9/userProfile/routes/user-route.js
@@ -6,4 +6,4 @@ router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
 router.post('/', userController.addUser);
 
-module.exports = router;
+module.exports = router
